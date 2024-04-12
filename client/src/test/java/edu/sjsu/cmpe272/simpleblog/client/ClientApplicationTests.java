6aa3e40deb29dee9a5f1040c47aecd853b094740
Sasi Kiran <sasikiranmenta@gmail.com>
2024-04-12 09:05:09 +0530

    updated changes

--- client/src/test/java/edu/sjsu/cmpe272/simpleblog/client/ClientApplicationTests.java
+++ client/src/test/java/edu/sjsu/cmpe272/simpleblog/client/ClientApplicationTests.java
@@ -1,13 +1,7 @@
 package edu.sjsu.cmpe272.simpleblog.client;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ClientApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
 }
--- server/src/test/java/edu/sjsu/cmpe272/simpleblog/server/ServerApplicationTests.java
+++ server/src/test/java/edu/sjsu/cmpe272/simpleblog/server/ServerApplicationTests.java
@@ -1,13 +1,7 @@
 package edu.sjsu.cmpe272.simpleblog.server;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ServerApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
 }
